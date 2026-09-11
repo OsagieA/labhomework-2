d40b66ef8da72ab11354726370a96e2d31ba7789
Osagie Akhagba <osagieafsw@gmail.com>
2026-09-11 09:59:11 -0400

    just committing what i missed to commit before lol

--- BankAccount.java
+++ BankAccount.java
@@ -7,7 +7,12 @@ public class BankAccount {
     }
     public void deposit(double amount) {
 // TODO: add the amount to the balance
+    if (amount > 0) {
+        balance += amount;
+
     }   
+    }
+
     public boolean withdraw(double amount) {
 // TODO:
 // Withdraw the requested amount if sufficient funds are available.
@@ -26,4 +31,25 @@ public class BankAccount {
     public String getAccountSummary() {
         return accountHolder + ": $" + balance;
     }
+
+public static void main(String[] args){
+    BankAccount acct = new BankAccount ("Osagie Akhagba",100.00 );
+    acct.deposit(50.00);
+    acct.withdraw(50.00);
+
+
+    System.out.println(acct.getAccountSummary());  
+
+    System.out.println(acct.withdraw(90.00));       
+    System.out.println(acct.getAccountSummary());
+
+    System.out.println(acct.withdraw(500.00));      
+    System.out.println(acct.getAccountSummary());
+
+
+
+
+
+}
+
 }
